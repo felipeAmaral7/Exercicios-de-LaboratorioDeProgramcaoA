6ecc1ef14a9c10ad5c0251ad82b9c8421bd64dfa
Felipe Amaral <150284514+felipeAmaral7@users.noreply.github.com>
2024-03-21 03:59:09 -0300

    Create questao07-soma.c
    Complete a função soma do código inicial. Ela recebe dois ponteiros para variáveis do tipo int e deve retornar um valor inteiro que corresponde à soma dessas duas variáveis.
    
    Quando o programa for executado, ele deverá ler dois números inteiros e imprimir a soma.
    
    Não altere a função main.
    
    // NÃO MODIFIQUE AS LINHAS ABAIXO
    #include <stdio.h>
    
    int soma(int *x, int *y);
    
    int main(void)
    {
        int x, y;
        scanf("%d%d", &x, &y);
        printf("%d\n", soma(&x, &y));
    }
    // NÃO MODIFIQUE AS LINHAS ACIMA
    
    
    int soma(int *x, int *y)
    {
        // Escreva seu código aqui
    }
    Exemplos de Entrada e Saída:
    
    Entrada:
    
    5 3
    
    Saída:	
    8

---
+++ Lista07-ponteiros-e-Funcoes/questao07-soma.c
@@ -0,0 +1,18 @@
+// NÃO MODIFIQUE AS LINHAS ABAIXO
+#include <stdio.h>
+
+int soma(int *x, int *y);
+
+int main(void)
+{
+    int x, y;
+    scanf("%d%d", &x, &y);
+    printf("%d\n", soma(&x, &y));
+}
+
+int soma(int *x, int *y){
+	
+	int soma = *x + *y;
+	
+	return soma;
+}
